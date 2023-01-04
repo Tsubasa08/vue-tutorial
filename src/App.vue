37305c36760f7cf0bc3539f67fd0d8973c5e9ddd
Tsubasa08 <46633705+Tsubasa08@users.noreply.github.com>
2023-01-05 00:43:39 +0900

    save

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Coffee msg="Coffee!!!" />
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Coffee from './components/Coffee.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Coffee,
+  data() {
+    return {
+      message: 'Hello World!',
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>{{ message }}</h1>
+  <div>{{ message.split('').reverse().join('') }}</div>
+</template>
